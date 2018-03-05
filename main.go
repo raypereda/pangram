fa753c9bf8bef8e2ffa8a4cc477423eb5fe6ebe3
Ray Pereda <raypereda@gmail.com>
2018-03-04 16:56:41 -0800

    add on constraint, letter a counts

--- main.go
+++ main.go
@@ -1,66 +1,180 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
-type sentence []byte
+type count []byte
 
-var s sentence
+var prefix = "blah blah has "
 
-var prefix = "a b sentence has"
-
-func countString(s string) sentence {
+func countLetters(s string) count {
 	c := make([]byte, 256)
-	var i int
-	for i = 0; i < len(s); i++ {
-		fmt.Printf("%x ", s[i])
+	for i := 97; i < 123; i++ {
+		c[i] = 1
+	}
+
+	for i := 0; i < len(s); i++ {
+		if s[i] < 97 || s[i] > 122 { // not lower case ASCII letter
+			continue
+		}
 		// increment count at that letter's index
 		// for example, "a" has index 65
+		// avoids having to calculating the index as s[i] - 65
+		// trading space for speed
 		c[s[i]]++
+		fmt.Printf("%q has count %d\n", s[i], c[s[i]])
+
 	}
 	return c
 }
 
-func iter(s sentence) sentence {
-	r := make([]byte, 256)
-
-	// maybe r is initialized with prefix string counts, pre-computed
-
-	var i int
-	for i = 0; i < len(s); i++ {
-		count := s[i]
-		switch count {
-		case 0:
-			// do nothing
+func countSentence(s count) count {
+	r := countLetters(prefix)
+	for i := 97; i < 122; i++ {
+		switch s[i] {
+		case 0: // "zero"
+			r['z']++
+			r['e']++
+			r['r']++
+			r['o']++
 		case 1: // "one"
-			r[65]++ // "o"
-			r[65]++ // "n"
-			r[65]++ // "e"
+			r['o']++
+			r['n']++
+			r['e']++
 		case 2: // "two"
-			r[65]++ // "t"
-			r[65]++ // "w"
-			r[65]++ // "o"
+			r['t']++
+			r['w']++
+			r['o']++
 		case 3: // "three"
-			r[65]++ // "t"
-			r[65]++ // "w"
-			r[65]++ // "o"
+			r['t']++
+			r['t']++
+			r['r']++
+			r['e']++
+			r['e']++
+		case 4: // "four"
+			r['f']++
+			r['o']++
+			r['u']++
+			r['r']++
+		case 5: // "five"
+			r['f']++
+			r['i']++
+			r['v']++
+			r['e']++
+		case 6: // "six"
+			r['s']++
+			r['i']++
+			r['x']++
+		case 7: // "seven"
+			r['s']++
+			r['e']++
+			r['v']++
+			r['e']++
+			r['n']++
+		case 8: // "eight"
+			r['e']++
+			r['i']++
+			r['g']++
+			r['h']++
+			r['t']++
+		case 9: // "nine"
+			r['n']++
+			r['i']++
+			r['n']++
+			r['e']++
+		default:
+			panic("count bigger than 9")
+
 		}
 	}
 	return r
 }
 
-func count(sentence) sentence {
-	return make([]byte, 2)
+// isSimple return true is all the letters are lower case or a space
+// ASCII code points, runes
+func isSimple(s string) bool {
+	for _, c := range s {
+		if c > 80 || c == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func printCount(c count) {
+	fmt.Println("letter countsssssss")
+	for i := 97; i < 123; i++ {
+		if c[i] == 0 {
+			continue
+		}
+		fmt.Printf("c[%q] = %d \n", i, c[i])
+	}
+}
+
+func spellout(c byte) string {
+	switch c {
+	case 0:
+		return "zero"
+	case 1:
+		return "one"
+	case 2:
+		return "two"
+	case 3:
+		return "three"
+	case 4:
+		return "four"
+	case 5:
+		return "five"
+	case 6:
+		return "six"
+	case 7:
+		return "seven"
+	case 8:
+		return "eight"
+	case 9:
+		return "nine"
+	default:
+		return "out of range"
+	}
+}
+
+func toString(c count) string {
+	var b bytes.Buffer
+	b.WriteString(prefix)
+
+	for i := 97; i < 98; i++ {
+		//	for i := 97; i < 122; i++ {
+		fmt.Printf("%q 's %s\n", i, spellout(c[i]))
+		num := spellout(c[i])
+		b.WriteString(num)
+		b.WriteString(" ")
+		letter := fmt.Sprintf("%qs", i)
+		withoutFirstQuote := letter[1:]
+		b.WriteString(withoutFirstQuote)
+
+	}
+
+	return b.String()
+}
+
+func print(c count) {
+	fmt.Println(toString(c))
 }
 
 func main() {
 	fmt.Println("Hello from Pangram Machine")
 
-	c := countString(prefix)
-	fmt.Println(c)
+	fmt.Println("prefix =", prefix)
 
-	fmt.Println(count(c))
+	c := countLetters(prefix)
+	printCount(c)
 
-	c2 := iter(c)
-	fmt.Println("c2 ", c2)
+	c2 := count(c)
+	print(c2)
+
+	// c2 := iter(c)
+	// fmt.Println("c2 ", c2)
 
 }
